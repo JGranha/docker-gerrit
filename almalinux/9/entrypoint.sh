83681ca359040bc3df181c7efe87806f76003a8f
Jose Granha <jose.granha@gmail.com>
2024-06-28 16:22:16 +0100

    Changes to support my images

--- almalinux/9/entrypoint.sh
+++ almalinux/9/entrypoint.sh
@@ -5,11 +5,25 @@ export JAVA_OPTS='--add-opens java.base/java.net=ALL-UNNAMED --add-opens java.ba
 if [ ! -d /var/gerrit/git/All-Projects.git ] || [ "$1" == "init" ]
 then
   echo "Initializing Gerrit site ..."
-  java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war init --batch --install-all-plugins -d /var/gerrit
-  java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
+  mkdir -p /var/gerrit/etc
   git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "-Djava.security.egd=file:/dev/./urandom"
   git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "--add-opens java.base/java.net=ALL-UNNAMED"
   git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "--add-opens java.base/java.lang.invoke=ALL-UNNAMED"
+  git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+  git config -f /var/gerrit/etc/gerrit.config --add plugins.allowRemoteAdmin "true"
+  git config -f /var/gerrit/etc/gerrit.config --add plugin.metrics-reporter-prometheus.prometheusBearerToken "token"
+  if [ -n "$AUTH_MODE" ];  then
+    git config -f /var/gerrit/etc/gerrit.config --add auth.type ${AUTH_MODE}
+  fi
+  if [ -n "$SERVER_ID" ];  then
+    git config -f /var/gerrit/etc/gerrit.config --add gerrit.serverId ${SERVER_ID}
+  fi
+  if [ -n "$INSTANCE_ID" ];  then
+    git config -f /var/gerrit/etc/gerrit.config --add gerrit.instanceId "${INSTANCE_ID}"
+  fi
+  java $JAVA_OPTS -jar /gerrit.war init --batch --no-auto-start --install-all-plugins --dev -d /var/gerrit
+  sleep 10
+  java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
 fi
 
 git config -f /var/gerrit/etc/gerrit.config gerrit.canonicalWebUrl "${CANONICAL_WEB_URL:-http://$HOSTNAME/}"
